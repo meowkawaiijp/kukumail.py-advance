a684cf84da2582641c579add7f8a20f40ce91194
zrpy <91083604+zrpy@users.noreply.github.com>
2024-11-22 20:28:17 +0900

    Update __init__.py

--- kukumail/__init__.py
+++ kukumail/__init__.py
@@ -1,12 +1,12 @@
-import requests
+import httpx
 import time
 import re
 import json
 import urllib
 
 class Client:
-    def __init__(self,session):
-        self.session=session
+    def __init__(self,proxy):
+        self.session=httpx.Client(proxy=proxy)
         self.session.headers = {
             "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'
